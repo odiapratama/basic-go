19ac6c14ebef75af2cf172b16125ee749323d998
telkomdev-odiapratama <odiapratama@gmail.com>
2021-03-16 20:02:55 +0700

    add interface empty

---
+++ interface-empty.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func greeting(greetingType int) interface{} {
+	switch greetingType {
+	case 1:
+		return "Hi"
+	case 2:
+		return 123
+	case 3:
+		return true
+	default:
+		return 0
+	}
+}
+
+func main() {
+	fmt.Println(greeting(1))
+	fmt.Println(greeting(2))
+	fmt.Println(greeting(3))
+	fmt.Println(greeting(99))
+}
