86ece1457b10325d85bc0216191756cde990571d
telkomdev-odiapratama <odiapratama@gmail.com>
2021-03-16 19:55:39 +0700

    add constant

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const phi = 3.14
+	fmt.Println("Phi =", phi)
+
+	const (
+		firstName = "Odia"
+		lastName  = "Pratama"
+	)
+
+	fmt.Println(firstName, lastName)
+}
