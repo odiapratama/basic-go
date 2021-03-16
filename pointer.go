73d27eb10ba2300f0e8da95a468e8d8516026bfa
telkomdev-odiapratama <odiapratama@gmail.com>
2021-03-16 19:59:29 +0700

    add pointer

---
+++ pointer.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// Address struct
+type Address struct {
+	City, Province, Region string
+}
+
+func changeRegionAddress(address *Address, city string) {
+	address.Region = city
+}
+
+func main() {
+	address1 := Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	address2 := &address1
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+
+	address2.City = "Bandung"
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+
+	*address2 = Address{"Malang", "Jawa Timur", "Indonesia"}
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+
+	address3 := new(Address)
+	address3.City = "Cilegon"
+	address3.Province = "Banten"
+	address3.Region = "Indonesia"
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+	fmt.Println(address3)
+
+	address4 := &Address{"Denpasar", "Bali", ""}
+	fmt.Println(address4)
+
+	changeRegionAddress(address4, "Indonesia")
+	fmt.Println(address4)
+}
